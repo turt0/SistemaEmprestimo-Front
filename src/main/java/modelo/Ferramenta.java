0c3a0c383190ed5747777ab184ff7189280a7474
Caio Candido <caio2006cunha@gmail.com>
2026-05-13 20:25:08 -0300

    fix: removed DAOs from Ferramenta

--- src/main/java/modelo/Ferramenta.java
+++ src/main/java/modelo/Ferramenta.java
@@ -1,7 +1,7 @@
 package modelo;
 
 import java.util.ArrayList;
-import dao.FerramentaDAO;
+
 
 public class Ferramenta extends ModeloBase {
 
@@ -10,7 +10,7 @@ public class Ferramenta extends ModeloBase {
      */
     private String marca;
     private double custo;
-    private FerramentaDAO dao;
+
 
     /**
      * construtor de objeto sem parametros
@@ -26,7 +26,7 @@ public class Ferramenta extends ModeloBase {
         super(id, nome);
         this.marca = marca;
         this.custo = custo;
-        dao = new FerramentaDAO();
+
     }
 
     /**
@@ -51,68 +51,5 @@ public class Ferramenta extends ModeloBase {
     @Override
     public String toString() {
         return super.toString() + "marca=" + marca + ", custo=" + custo;
-    }
-
-    
-    /**
-     * retorna a lista de ferramentas
-     */
-    public ArrayList<Ferramenta> getMinhaLista() {
-        return dao.getMinhaLista();
-    }
-
-    /**
-     * cadastra nova ferramenta
-     */
-    public boolean insertFerramentaBD(String nome, String marca, double custo) {
-        int id = dao.maiorID() + 1;
-        Ferramenta objeto = new Ferramenta(id, nome, marca, custo);
-        dao.insertFerramentaBD(objeto);
-        return true;
-    }
-
-    /**
-     * deleta uma ferramenta especifica pelo id
-     */
-    public boolean deleteFerramentaBD(int id) {
-        dao.deleteFerramentaBD(id);
-        return true;
-    }
-
-    /**
-     * edita uma ferramenta especifica pelo id
-     */
-    public boolean updateFerramentaBD(int id, String nome, String marca, double custo) {
-        Ferramenta objeto = new Ferramenta(id, nome, marca, custo);
-        dao.updateFerramentaBD(objeto);
-        return true;
-    }
-
-    /**
-     * procura o indice de objeto da minhaLista que tem o id enviado
-     */
-    private int procuraIndice(int id) {
-        int indice = -1;
-        for (int i = 0; i < dao.minhaLista.size(); i++) {
-            if (dao.minhaLista.get(i).getId() == id) {
-                indice = i;
-            }
-        }
-        return indice;
-    }
-
-    /**
-     * carrega dados de uma ferramenta especifica pelo id
-     */
-    public Ferramenta carregaFerramenta(int id) {
-        int indice = this.procuraIndice(id);
-        return dao.minhaLista.get(indice);
-    }
-
-    /**
-     * retorna o maior id do BD
-     */
-    public int maiorID() {
-        return dao.maiorID();
     }
 }
