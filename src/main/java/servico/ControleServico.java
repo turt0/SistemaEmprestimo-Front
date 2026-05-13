01832d572d4290fc6dae39b6d148b40fe3754613
Caio Candido <caio2006cunha@gmail.com>
2026-05-13 19:48:42 -0300

    feat: add url base and namespace to ControleServico

--- src/main/java/servico/ControleServico.java
+++ src/main/java/servico/ControleServico.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package servico;
 
-/**
- *
- * @author Caio
- */
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
 public class ControleServico {
+    
+     /**
+     * URL base do servidor de webservices. Centralizada aqui para que
+     * uma mudança de host/porta exija alteração em apenas um lugar.
+     */
+    private static final String URL_BASE = "http://localhost:8080/webservice_emprestimo_jaxws_soap_tomcat";
+    
+    /**
+     * Namespace dos serviços. Como as classes do backend estão no pacote
+     * "servico", o namespace gerado pelo JAX-WS é "http://servico/".
+     */
+    private static final String NAMESPACE = "http://servico/";
     
 }
