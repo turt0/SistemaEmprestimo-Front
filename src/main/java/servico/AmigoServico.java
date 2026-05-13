f16d32e890ee169e5703a9a4f54631d73f1601b9
Caio Candido <caio2006cunha@gmail.com>
2026-05-13 19:55:03 -0300

    feat: add list method to AmigoServico

--- src/main/java/servico/AmigoServico.java
+++ src/main/java/servico/AmigoServico.java
@@ -7,6 +7,13 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import modelo.Amigo;
 
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface AmigoServico {
     
+    /**
+     * Retorna a lista de todos os amigos cadastrados.
+     */
+    @WebMethod(operationName = "listar")
+    ArrayList<Amigo> listar();
 }
